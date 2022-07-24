a20e9a17f9c06f6463fccc2b4e17ce7177a52747
Chris Griffing <cmgriffing@gmail.com>
2022-07-24 12:53:45 -0700

    fix: try a simpler github service container name

--- scripts/generate-sdks-ci.ts
+++ scripts/generate-sdks-ci.ts
@@ -68,7 +68,7 @@ async function main() {
     console.log("Waiting for server to boot... \n");
     await asyncTimeout(10000);
 
-    const GEN_IP = "generator-service";
+    const GEN_IP = "generator";
 
     for (let i = 0; i < languages.length; i++) {
       const language = languages[i];
